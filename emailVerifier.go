fc9c56909fe26e1dd82a1e6573ff847b246426e1
James Duerden <james.duerden@marketdojo.com>
2016-12-01 23:36:58 +0000

    Fixed imports

--- emailVerifier.go
+++ emailVerifier.go
@@ -1,5 +1,12 @@
 package emailVerifier
 
+import (
+	"errors"
+	"net"
+	"net/textproto"
+	"strings"
+)
+
 // FindEmail takes a first, last and company name, finds MX and mail server addresses,
 // creates possibilities for email addresses and tries them against the server.
 func FindEmail(firstName string, lastName string, companyName string) (string, error) {
@@ -8,4 +15,31 @@ func FindEmail(firstName string, lastName string, companyName string) (string, e
 		return "", err
 	}
 	return test[0], nil
+}
+
+// VerifyEmail takes an email and checks whether the related MX server for the host agrees that it exists.
+func VerifyEmail(email string) error {
+	host := strings.Split(email, "@")[1]
+
+	res, err := net.LookupMX(host)
+	if err != nil {
+		return errors.New("Incorrect Host Address")
+	}
+	mxServer := strings.TrimRight(res[0].Host, ".")
+	conn, err := textproto.Dial("tcp", mxServer+":25")
+	if err != nil {
+		return err
+	}
+
+	defer conn.Close()
+
+	if err := setupMX(conn, email); err != nil {
+		return err
+	}
+
+	if err := checkResponse(conn, "rcpt to: <"+email+">", 250); err != nil {
+		return errors.New("Recipient " + email + " invalid: " + err.Error())
+	}
+
+	return nil
 }
--- tryEmails.go
+++ tryEmails.go
@@ -4,7 +4,6 @@ import "net/textproto"
 import "errors"
 
 import "strings"
-import "net"
 
 func checkResponse(conn *textproto.Conn, request string, code int) error {
 	if request != "" {
@@ -19,33 +18,6 @@ func checkResponse(conn *textproto.Conn, request string, code int) error {
 	if err != nil {
 		return errors.New("Did not get intended response (" + string(code) + "): " + err.Error())
 	}
-	return nil
-}
-
-// VerifyEmail takes an email and checks whether the related MX server for the host agrees that it exists.
-func VerifyEmail(email string) error {
-	host := strings.Split(email, "@")[1]
-
-	res, err := net.LookupMX(host)
-	if err != nil {
-		return errors.New("Incorrect Host Address")
-	}
-	mxServer := strings.TrimRight(res[0].Host, ".")
-	conn, err := textproto.Dial("tcp", mxServer+":25")
-	if err != nil {
-		return err
-	}
-
-	defer conn.Close()
-
-	if err := setupMX(conn, email); err != nil {
-		return err
-	}
-
-	if err := checkResponse(conn, "rcpt to: <"+email+">", 250); err != nil {
-		return errors.New("Recipient " + email + " invalid: " + err.Error())
-	}
-
 	return nil
 }
 
